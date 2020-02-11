1714f52df4f7c582ea733a0dfaf0e8f8d56c4e26
Tad Preston <tadpreston@gmail.com>
2020-02-11 15:46:34 -0600

    added the login stuff

--- pages/login.js
+++ pages/login.js
@@ -45,7 +45,60 @@ function Login() {
     }
   }
 
-  return <>Login</>
+  return <>
+    <Message
+      attached
+      icon="privacy"
+      header="Welcome Back!"
+      content="Log in with email and password"
+      color="blue"
+    />
+    <Form error={Boolean(error)} loading={loading} onSubmit={handleSubmit}>
+      <Message
+        error
+        header="Oops!"
+        content={error}
+      />
+      <Segment>
+        <Form.Input
+          fluid
+          icon="envelope"
+          iconPosition="left"
+          label="Email"
+          placeholder="Email"
+          name="email"
+          type="email"
+          value={user.email}
+          onChange={handleChange}
+        />
+        <Form.Input
+          fluid
+          icon="lock"
+          iconPosition="left"
+          label="Password"
+          placeholder="Password"
+          name="password"
+          type="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <Button
+          disabled={disabled || loading}
+          icon="sign in"
+          type="submit"
+          color="orange"
+          content="Login"
+        />
+      </Segment>
+    </Form>
+    <Message attached="botton" warning>
+      <Icon name="help" />
+      New user?{"  "}
+      <Link href="/signup">
+        <a>Sign up here</a>
+      </Link>{"  "}instead.
+    </Message>
+  </>;
 }
 
 export default Login;
