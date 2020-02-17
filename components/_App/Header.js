49a6c3f7cbd76b1ab250e53feeb3d0ab213cb16b
Tad Preston <tadpreston@gmail.com>
2020-02-17 15:46:30 -0600

    added a drop dowm menu on the header

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,4 +1,4 @@
-import { Menu, Container, Image, Icon } from 'semantic-ui-react';
+import { Menu, Container, Image, Icon, Dropdown } from 'semantic-ui-react';
 import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import { handleLogout } from "../../utils/auth";
@@ -35,13 +35,35 @@ function Header({ user }) {
         </Link>
 
         { user ?  (<>
-          <Menu.Item onClick={handleLogout} header>
-            <Icon
-              name="sign out"
-              size="large"
-            />
-            Logout
-          </Menu.Item>
+          <Dropdown item text={user.name}>
+            <Dropdown.Menu>
+              <Link href="/user">
+                <Dropdown.Item header>
+                  <Icon
+                    name="user"
+                    size="large"
+                  />
+                  Profile
+                </Dropdown.Item>
+              </Link>
+              <Link href="/organization">
+                <Dropdown.Item header>
+                  <Icon
+                    name="globe"
+                    size="large"
+                  />
+                  Organization
+                </Dropdown.Item>
+              </Link>
+              <Dropdown.Item onClick={handleLogout} header>
+                <Icon
+                  name="sign out"
+                  size="large"
+                />
+                Logout
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </>) :
         (<>
           <Link href="/login">
